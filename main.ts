005ec7162f1915bc69891b3cd606219799a6aa41
ksydnor <kamausydnor2004@gmail.com>
2024-05-15 00:53:52 -0400

    Update main.ts
    Initial Commit

--- main.ts
+++ main.ts
@@ -1,134 +1,106 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Notice, App, Modal, MarkdownView } from 'obsidian';
+class JsonModal extends Modal {
+	private jsonData: string = '';
+	onCloseCallback: () => void = null;
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
-
+//modal logic
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		contentEl.createEl('h2', { text: 'Enter your JSON data below:' });
+
+		const textarea = contentEl.createEl('textarea', {
+			attr: { id: 'json-data', rows: '10' },
+		}) as HTMLTextAreaElement;
+		textarea.style.width = '100%';
+		textarea.style.resize = 'vertical';
+
+		const confirmButton = contentEl.createEl('button', { text: 'Confirm' });
+		confirmButton.style.marginTop = '10px';
+		confirmButton.onclick = () => {
+			this.jsonData = textarea.value.trim();
+			if (!this.jsonData) {
+				new Notice('Please enter valid JSON data.');
+				return;
+			}
+
+			try {
+				JSON.parse(this.jsonData);
+				this.close();
+			} catch (error) {
+				new Notice('Error parsing JSON data. Please check your input.');
+			}
+		};
+
+		setTimeout(() => {
+			textarea.focus();
+		}, 0);
 	}
 
 	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		this.contentEl.empty();
+		this.onCloseCallback?.();
+	}
+
+	getData(): string {
+		return this.jsonData;
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export default class MyPlugin extends Plugin {
+	private modal: JsonModal;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	async onload() {
+		console.log('Loading MyPlugin');
+
+		this.addCommand({
+			id: 'replace-data',
+			name: 'Replace Data in Markdown',
+			callback: () => this.openJsonModal(),
+		});
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	openJsonModal() {
+		this.modal = new JsonModal(this.app);
+		this.modal.open();
+		this.modal.onCloseCallback = () => {
+			const jsonData = this.modal.getData();
+			if (jsonData) {
+				this.replaceDataInActiveMarkdown(jsonData);
+				console.log('JSON Data:', jsonData);
+				new Notice('JSON Data Received Successfully');
+			}
+		};
+	}
+	// replacement logic
+	replaceDataInActiveMarkdown(jsonData: string) {
+		const activeLeaf = this.app.workspace.activeLeaf;
+		if (activeLeaf?.view instanceof MarkdownView) {
+			const editor = activeLeaf.view.editor;
+			let docContent = editor.getValue();
 
-		containerEl.empty();
+			try {
+				const json = JSON.parse(jsonData);
+				Object.keys(json).forEach(key => {
+					const regex = new RegExp(`{{${key}}}`, 'g');
+					docContent = docContent.replace(regex, json[key]);
+				});
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+				editor.setValue(docContent);
+				new Notice('Document updated with JSON data.');
+			} catch (error) {
+				new Notice('Error parsing JSON data. Please check your input.');
+				console.error('Error parsing JSON data:', error);
+			}
+		} else {
+			new Notice('No active Markdown document to update.');
+		}
+	}
+
+	onunload() {
+		console.log('Unloading MyPlugin');
 	}
 }
